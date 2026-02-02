a7d8e8f33bb36295c38c40148c43398b10482e78
Marcos Leal <marcosleal@gmail.com>
2026-02-02 16:15:37 -0500

    Creating Token and useCase for Company

--- src/main/java/com/marcos/open_jobs/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/marcos/open_jobs/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,49 @@
 package com.marcos.open_jobs.modules.company.useCases;
 
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.marcos.open_jobs.modules.company.dto.AuthCompanyDTO;
+import com.marcos.open_jobs.modules.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
 
 @Service
 public class AuthCompanyUseCase {
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+                () -> {
+                    throw new UsernameNotFoundException("Username/Password incorrect.");
+                }
+        );
+        // Check if password is the same
+        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+        // if not equal, return error
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+        // if equal, return token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("OpenJobs")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
+    }
 }
